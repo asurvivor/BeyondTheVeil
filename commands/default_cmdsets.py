189c5f3534b203612b5b3c17ef7e44151f4c688e
Adam C <asurvivor@users.noreply.github.com>
2018-06-10 11:22:24 -0600

    Added Multidesc functionality from contribs
    Imported multidesc class in default command sets. Added the line to add command to character commands.

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -16,6 +16,7 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 from evennia import default_cmds
 from evennia.contrib import dice
+from evennia.contrib.multidescer import CmdMultiDesc
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
@@ -34,6 +35,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         # any commands you add below will overload the default ones.
         #
         self.add(dice.CmdDice())
+        self.add(CmdMultiDesc())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
